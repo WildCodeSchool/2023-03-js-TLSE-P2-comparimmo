8b866fe3c65d166261a40554a9b07e405e855511
Hugo Carnazza <hugo.azzanrac@gmail.com>
2023-04-20 15:15:50 +0200

    S01US01_Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+// eslint-disable-next-line import/no-unresolved
+import logomobile from "@assets/img/Logo-mobile.png";
+// eslint-disable-next-line import/no-unresolved
+import logodesktop from "@assets/img/Logo-Desktop.png";
+import "./Navbar.scss";
+
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <section className="navbar">
+      <img className="logo-mobile" src={logomobile} alt="logo-mobile" />
+      <img className="logo-desktop" src={logodesktop} alt="logo-desktop" />
+      <div>
+        <span
+          id="burger_icon"
+          className={showMenu ? "animationOpen" : "no_animation"}
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex="0"
+        >
+          <span />
+          <span />
+          <span />
+          <span />
+          <span />
+        </span>
+        <div
+          id="nav_links"
+          className={showMenu ? "burger_open" : "burger_close"}
+        >
+          <ul>
+            <NavLink exact to="/" activeClassName="active">
+              <li>Home</li>
+            </NavLink>
+            <NavLink to="/about" activeClassName="active">
+              <li>About Us</li>
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;

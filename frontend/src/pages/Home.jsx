4e5891f7b359099eed3000b8e70f68be199067f2
aimach <schneider.marieh@gmail.com>
2023-04-25 12:18:10 +0200

    test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Carousel from "../components/Carousel/Carousel";
+// eslint-disable-next-line import/no-unresolved
+import Carousel from "@components/Carousel/Carousel";
 
 function Home() {
   return <Carousel />;

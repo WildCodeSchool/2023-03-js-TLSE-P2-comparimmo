689de264e0737754b2184ff0a8881b955b2d46d8
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-04-26 14:22:33 +0200

    S02US4_map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./assets/styles/index.scss";
+// eslint-disable-next-line import/no-unresolved
 import Footer from "./components/Footer/Footer";
+// eslint-disable-next-line import/no-unresolved
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";

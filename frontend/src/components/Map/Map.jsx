3c8ccc1ca64acfa1beda4a4c67893810de0b7f59
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-04-26 09:41:14 +0200

    S02US4_map

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,39 +1,50 @@
 import React, { useEffect, useState } from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { MapContainer, TileLayer, Popup, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
 import styles from "./Map.module.scss";
 
-export default function Map() {
-  const [polygons, setPolygons] = useState([]);
-  const [libtypbien, setLibtypbien] = useState([]);
-  const [valeurfonc, setValeurfonc] = useState([]);
-  const [datemut, setDatemut] = useState("");
-  const [sbati, setSbati] = useState("");
-  const [idmutations, setIdmutations] = useState([]);
-  const [codeInsee, setCodeInsee] = useState("31555");
-  const [sterr, setSterr] = useState([]);
+export default function Map({
+  sbati,
+  datemut,
+  libtypbien,
+  idmutations,
+  polygons,
+  sterr,
+  valeurfonc,
+  codeInsee,
+}) {
+  const [polygonsState, setPolygonsState] = useState(polygons || "");
+  const [libtypbienState, setLibtypbienState] = useState(libtypbien || "");
+  const [valeurfoncState, setValeurfoncState] = useState(valeurfonc || "");
+  const [datemutState, setDatemutState] = useState(datemut || "");
+  const [sbatiState, setSbatiState] = useState(sbati || "");
+  const [idmutationsState, setIdmutationsState] = useState(idmutations || "");
+  const [codeInseeState, setCodeInseeState] = useState(codeInsee || "");
+  const [sterrState, setSterrState] = useState(sterr || "");
 
-  /* changement code insee bouton */
+  /* change code insee button */
   const handleCodeInseeChange = (event) => {
-    setCodeInsee(event.target.value);
+    setCodeInseeState(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
+  // Retrieving data from the API
   useEffect(() => {
     axios
       .get(
-        `https://apidf-preprod.cerema.fr/dvf_opendata/geomutations/?code_insee=${codeInsee}&page_size=500&codtypbien=121,111`
+        `https://apidf-preprod.cerema.fr/dvf_opendata/geomutations/?code_insee=${codeInseeState}&page_size=500&codtypbien=121,111`
       )
       .then((response) => {
         const filteredFeatures = response.data.features.filter(
           (feature) => feature.geometry !== null
         );
 
-        // inversement des coordonées geographiques //
+        // reverse geographic coordinates //
 
         const flatPolygons = filteredFeatures.map((feature) => {
           const flatArray = feature.geometry.coordinates.flat();
@@ -43,21 +54,24 @@ export default function Map() {
 
         const { features } = response.data;
 
-        setPolygons(flatPolygons);
-        setLibtypbien(features.map((feature) => feature.properties.libtypbien));
-        setValeurfonc(features.map((feature) => feature.properties.valeurfonc));
-        setDatemut(features.map((feature) => feature.properties.datemut));
-        setSbati(features.map((feature) => feature.properties.sbati));
-        setIdmutations(
+        setPolygonsState(flatPolygons);
+        setLibtypbienState(
+          features.map((feature) => feature.properties.libtypbien)
+        );
+        setValeurfoncState(
+          features.map((feature) => feature.properties.valeurfonc)
+        );
+        setDatemutState(features.map((feature) => feature.properties.datemut));
+        setSbatiState(features.map((feature) => feature.properties.sbati));
+        setIdmutationsState(
           features.map((feature) => feature.properties.idmutation)
         );
-        setSterr(features.map((feature) => feature.properties.sterr));
+        setSterrState(features.map((feature) => feature.properties.sterr));
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [codeInsee]);
-
+  }, [codeInseeState]);
   return (
     <div className={`${styles.mapContent}  `}>
       <form onSubmit={handleSubmit}>
@@ -65,77 +79,80 @@ export default function Map() {
           Code INSEE :
           <input
             type="text"
-            value={codeInsee}
+            value={codeInseeState}
             onChange={handleCodeInseeChange}
           />
         </label>
         <button type="submit">Changer</button>
       </form>
-      {/* contenu de la carte */}
+      {/*  map settings */}
       <MapContainer
         className={`${styles.map}`}
         center={[43.6, 1.433333]}
-        zoom={12}
+        zoom={5}
         maxZoom={18}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="COMPAR'IMMO"
         />
-        {/* affichage du contenu des objets sur la carte  */}
-        {polygons.map((polygon, index) => (
-          <Polygon key={idmutations[index]} positions={polygon}>
+        {/* display of object content on the map: object position and characteristics */}
+        {polygonsState.map((polygon, index) => (
+          <Polygon key={idmutationsState[index]} positions={polygon}>
             <Popup>
-              {datemut[index] ? (
+              {datemutState[index] ? (
                 <p>
                   <strong>Date de mutation:</strong>
                   <br />
-                  {new Date(datemut[index]).toLocaleDateString("fr-FR")}
+                  {new Date(datemutState[index]).toLocaleDateString("fr-FR")}
                 </p>
               ) : (
                 ""
               )}
-              {libtypbien[index] ? (
+              {libtypbienState[index] ? (
                 <p>
                   <strong>Type de bien:</strong>
                   <br />
-                  {libtypbien[index].toLocaleLowerCase()}
+                  {libtypbienState[index].toLocaleLowerCase()}
                 </p>
               ) : (
                 ""
               )}
-              {valeurfonc[index] ? (
+              {valeurfoncState[index] ? (
                 <p>
                   <strong>Valeur foncière:</strong>
                   <br />
-                  {Number(valeurfonc[index]).toLocaleString("fr-FR")} €
+                  {Number(valeurfoncState[index]).toLocaleString("fr-FR")} €
                 </p>
               ) : (
                 ""
               )}
-              {sbati[index] ? (
+              {sbatiState[index] ? (
                 <p>
                   <strong>Surface :</strong>
                   <br />
-                  {sbati[index]} m²
+                  {sbatiState[index]} m²
                 </p>
               ) : (
                 ""
               )}
-              {sterr[index] > 0 ? (
+              {sterrState[index] > 0 ? (
                 <p>
                   <strong>Surface du terrain:</strong>
                   <br />
-                  {sterr[index]} m²
+                  {sterrState[index]} m²
                 </p>
               ) : (
                 ""
               )}
-              {sbati[index] > 0 && valeurfonc[index] > 0 ? (
+              {sbatiState[index] > 0 && valeurfoncState[index] > 0 ? (
                 <p>
                   <strong>Prix au m²:</strong>
                   <br />
-                  {Number(valeurfonc[index] / sbati[index]).toFixed(0)} €/m²
+                  {Number(valeurfoncState[index] / sbatiState[index]).toFixed(
+                    0
+                  )}{" "}
+                  €/m²
                 </p>
               ) : (
                 ""
@@ -148,22 +165,24 @@ export default function Map() {
   );
 }
 
-// Map.propTypes = {
-//   sbati: PropTypes.string,
-//   datemut: PropTypes.string,
-//   libtypbien: PropTypes.string,
-//   idmutations: PropTypes.string,
-//   polygons: PropTypes.string,
-//   codeInsee: PropTypes.string,
-//   sterr: PropTypes.string,
-// };
+Map.propTypes = {
+  sbati: PropTypes.string,
+  datemut: PropTypes.string,
+  libtypbien: PropTypes.string,
+  idmutations: PropTypes.string,
+  polygons: PropTypes.string,
+  codeInsee: PropTypes.string,
+  sterr: PropTypes.string,
+  valeurfonc: PropTypes.string,
+};
 
-// Map.defaultProps = {
-//   sbati: "",
-//   datemut: "",
-//   codeInsee: "31555",
-//   libtypbien: [],
-//   idmutations: [],
-//   polygons: [],
-//   sterr: [],
-// };
+Map.defaultProps = {
+  sbati: "",
+  datemut: "",
+  codeInsee: "31555",
+  libtypbien: [],
+  idmutations: [],
+  polygons: [],
+  sterr: [],
+  valeurfonc: [],
+};

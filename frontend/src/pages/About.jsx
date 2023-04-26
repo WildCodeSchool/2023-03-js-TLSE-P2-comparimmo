f92933125a9336cb541a0289057c8c48900f0a3e
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-04-26 16:19:59 +0200

    S03US13_about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import "../assets/styles/index.scss";
+import "./About.scss";
+// eslint-disable-next-line import/no-unresolved
+import minilogo from "@assets/img/minilogo.jpg";
+import cedric from "@assets/img/cedric.jpg";
+import mathieu from "@assets/img/mathieu.jpg";
 
 function About() {
   return (
@@ -8,50 +14,69 @@ function About() {
       </div>
       <section>
         <div className="aboutComparimmo">
-          <img src="" alt="" />
-          <h2>COMPAR'IMMO</h2>
+          <div className="d-flex">
+            <img src={minilogo} alt="" />
+            <h2>COMPAR'IMMO</h2>
+          </div>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
-            consequuntur doloribus repellat quos, saepe facere quae quisquam
-            molestias deserunt non delectus libero exercitationem dolorum hic
-            ipsam dolorem enim nihil omnis fugit adipisci, voluptate neque
-            sapiente cupiditate est. Necessitatibus, placeat fugiat sapiente
-            adipisci provident culpa? Culpa ad tempore eos debitis inventore!
+            Bienvenue sur notre site internet dédié à la comparaison des valeurs
+            foncières des maisons et appartements sur les différentes communes
+            de France. Grâce à notre outil de comparaison, vous pouvez
+            facilement trouver le prix au mètre carré dans différentes villes et
+            communes, ainsi que localiser les ventes de biens immobiliers grâce
+            à notre carte interactive. Notre objectif est de vous fournir les
+            informations les plus précises et utiles pour vous aider à prendre
+            des décisions éclairées en matière d'achat ou de vente de biens
+            immobiliers en France.
           </p>
         </div>
         <div className="aboutAPi">
-          <img src="" alt="" />
-          <h2> Ressources utilisées </h2>
+          <div className="d-flex">
+            <img src={minilogo} alt="" />
+            <h2> Ressources utilisées </h2>
+          </div>
+          <p>
+            Pour réaliser ce site internet, plusieurs sources (notamment pour
+            les données) ont été utilisées.
+          </p>
           <ul>
-            <p>
-              Pour réaliser ce site internet, plusieurs sources (notamment pour
-              les données) ont été utilisées.
-            </p>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
+            <li>
+              <a href="https://datafoncier.cerema.fr/donnees/autres-donnees-foncieres/dvfplus-open-data">
+                DVF+ open-data par (CEREMA)
+              </a>
+            </li>
+            <li>
+              <a href="https://leafletjs.com/">LEAFLET MAP</a>
+            </li>
+            <li>
+              {" "}
+              <a href="https://api.gouv.fr/documentation/api-geo">
+                API Découpage Administratif - (API Geo)
+              </a>
+            </li>
           </ul>
         </div>
 
         <div className="aboutDev">
-          <img src="" alt="" />
-          <h2>L'équipe de dévelppeurs</h2>
+          <div className="teamDev">
+            <img src={minilogo} alt="Logo Compar'Immo" />
+            <h2>L'équipe de développeurs</h2>
+          </div>
           <p>L’équipe de développeur est composée de : </p>
           <div className="dev">
-            <img src="" alt="" />
+            <img src="" alt="Hugo" />
             <h2>Hugo CARNAZZA</h2>
           </div>
           <div className="dev">
-            <img src="" alt="" />
+            <img src="" alt="Antoine" />
             <h2>Antoine CASSAGNE</h2>
           </div>
           <div className="dev">
-            <img src="" alt="" />
+            <img src={mathieu} alt="Mathieu" />
             <h2>Matthieu GUINET</h2>
           </div>
           <div className="dev">
-            <img src="" alt="" />
+            <img src={cedric} alt="Cédric" />
             <h2>Cédric PALACIO-VIDAL</h2>
           </div>
         </div>
